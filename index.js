db2df1479d33ed255d2a98f2b9c6e6cb4cb971ab
Caleb Meador <cmeador@IT0009918.irving1.lcl>
2018-10-16 11:02:43 -0500

    bigger and better triangles. still need to test more combos

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const Bear = require('./app/models/bear')
 const htmlPath = path.join(__dirname, 'html')
 const port = process.env.PORT || 8080
 
-const spacing = 50
+const spacing = 100 
 const gridWidth = 1000
 const gridHeight = 1000
 const clear = { color: { r: 0, g: 0, b: 0, a: 0 } }
-const defaultLineWidth = 4
+const defaultLineWidth = 10
 
 lines_aa = {}
 tri_aa = {}
@@ -157,57 +157,64 @@ function findClosed (line) {
   testLines = []
   triangles = [] 
   lineKey = [line.x1,line.y1,line.x2,line.y2].join(',') 
-  lineNodes = [[line.x1, line.y1],[line.x2, line.y2]]
+  lineNodes = [[line.x1, line.y1],[line.x2, line.y2]].sort()
   lineSlope = (line.y2-line.y1)/(line.x2 - line.x1)
-	console.log("lineNodes", lineNodes)
-  lineNodes.forEach(function (node) {
-		console.log("here")
-    Object.keys(lines_aa).forEach(function (key) {
+
+  console.log("lineNodes", lineNodes)
+	
+  Object.keys(lines_aa).forEach(function (key) {
     if (lineKey != key){
-        key_coords = key.split(',');
-        keyNodes = [[parseInt(key_coords[0]), parseInt(key_coords[1])],[parseInt(key_coords[2]),parseInt(key_coords[3])]]
-        if( (node[0] === keyNodes[0][0] && node[1] === keyNodes[0][1])){// || (node[0] === keyNodes[1][0] && node[1] === keyNodes[1][1])){ 
-		console.log("testline", keyNodes)
-          testLines.push(keyNodes)      
-        }
+      key_coords = key.split(',');
+      keyNodes = [[parseInt(key_coords[0]), parseInt(key_coords[1])],[parseInt(key_coords[2]),parseInt(key_coords[3])]]
+	    
+      if(  equalNodes(lineNodes[0], keyNodes[0]) 
+	   || equalNodes(lineNodes[1], keyNodes[0])
+           || equalNodes(lineNodes[0], keyNodes[1])
+	   || equalNodes(lineNodes[1], keyNodes[1])
+      ){
+      	console.log("testline", keyNodes)
+        testLines.push(keyNodes.sort())      
       }
-    });
+    }
   })
-	console.log(testLines.length)
+
+  console.log("testlines.length", testLines.length)
   if (testLines.length > 1) { 
     testLines.forEach(function(testLine1){
-    testLineSlope1 = (testLine1[1][1]-testLine1[0][1])/(testLine1[1][0]-testLine1[0][0])
+
+     testLineSlope1 = (testLine1[1][1]-testLine1[0][1])/(testLine1[1][0]-testLine1[0][0])
+
      testLines.forEach(function(testLine2){
 
-    testLineSlope2 = (testLine2[1][1]-testLine2[0][1])/(testLine2[1][0]-testLine2[0][0])
+       testLineSlope2 = (testLine2[1][1]-testLine2[0][1])/(testLine2[1][0]-testLine2[0][0])
 
-      if (lineKey != testLine1 && testLine1 != testLine2 && lineSlope != testLineSlope1 && testLineSlope1 != testLineSlope2){
-	  console.log("lineNodes", lineNodes, "testLine1", testLine1, "testLine2", testLine2)    
-	     
-        if( (containsNode(lineNodes, testLine1[0]) || containsNode(lineNodes, testLine1[1]))
-	     && (containsNode(lineNodes, testLine2[0]) || containsNode(lineNodes, testLine2[1]))){
-	    //&& (containsNode(testLine1, testLine2[0]) || containsNode(testLine1, testLine2[1]))
-	    //&& (containsNode(testLine2, lineNodes[0]) || containsNode(testLine2, lineNodes[1]))){
-            triangleNodesSet = setOfNodes([lineNodes[0], lineNodes[1], testLine1[0], testLine1[1], testLine2[0], testLine2[1]] )
-	     triangles.push(flatten(triangleNodesSet))
-		console.log("triangles.push", flatten(triangleNodesSet))
-	     }
-	     
-        //lineLen = Math.sqrt(Math.pow(line.x2-line.x1, 2) + Math.pow(line.y2-line.y1, 2))
-        //testLineLen1 = Math.sqrt(Math.pow(testLine1[2]-testLine1[0], 2) + Math.pow(testLine1[3]-testLine1[1], 2))
-        //testLineLen2 = Math.sqrt(Math.pow(testLine2[2]-testLine2[0], 2) + Math.pow(testLine2[3]-testLine2[1], 2))
-        //if ((lineLen + testLineLen1 > testLineLen2) && (lineLen + testLineLen2 > testLineLen1) && (testLineLen1 + testLineLen2 > lineLen)){
-        //  lineNodes.push([parseInt(testLine1[0]), parseInt(testLine1[1])], [parseInt(testLine1[2]), parseInt(testLine1[3])],[parseInt(testLine2[0]), parseInt(testLine2[1])], [parseInt(testLine2[2]), parseInt(testLine2[3])])
-        //  lineNodesSet = setOfNodes(lineNodes)
-        //  triangles.push(flatten(lineNodesSet))
-        //        console.log(lineNodesSet)
-        //}
-      }
+       if (lineKey != testLine1 
+               && testLine1 != testLine2 
+	       && lineKey != testLine2
+               && lineSlope != testLineSlope1 
+               && testLineSlope1 != testLineSlope2
+	       && lineSlope != testLineSlope2
+       ){
+         if( (equalNodes(lineNodes[0], testLine2[0]) && equalNodes(lineNodes[1], testLine1[0]) && equalNodes(testLine1[1], testLine2[1]))
+          || (equalNodes(lineNodes[0], testLine1[0]) && equalNodes(lineNodes[1], testLine2[0]) && equalNodes(testLine1[1], testLine2[1]))
+          || (equalNodes(testLine2[0], testLine1[0]) && equalNodes(lineNodes[1], testLine2[0]) && equalNodes(testLine1[1], lineNodes[1]))
+         ){
+             triangleNodesSet = setOfNodes([lineNodes[0], lineNodes[1], testLine1[0], testLine1[1], testLine2[0], testLine2[1]] )
+              triangles.push(flatten(triangleNodesSet))
+         	console.log("triangles.push", flatten(triangleNodesSet))
+         }
+       }	     
      })
     })
   }
   return triangles
 }
+
+//node as [x,y]
+function equalNodes(node1, node2){
+  return node1[0] === node2[0] && node1[1] === node2[1] 
+}
+
 function containsNode(arr, node){
   test = false
   arr.forEach(function(n){
@@ -217,6 +224,7 @@ function containsNode(arr, node){
   console.log("contains", arr, node, test )
   return test 
 }
+
 function setOfNodes(nodeArray){
 	console.log("nodeArrayLength", nodeArray.length)
 	console.log("nodeArrayBefore", nodeArray)
@@ -232,119 +240,11 @@ function setOfNodes(nodeArray){
 	console.log("nodeArrayAfter", nodeArray)
  return nodeArray
 }
-//function findClosed (line) {
-//  slope = ( line.y2 - line.y1 ) / ( line.x2 - line.x1 )
-//  triangle = null
-//  //line slope is -1, "forwardslash"
-//  if ( slope === -1 ){ 
-//    if( findLine (line, 0, spacing, 0, 0) && findLine (line, spacing, 0, 0, 0) ){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.max(line.y1, line.y2)]  
-//        console.log("slope -1 x+y+ connected", triangle)
-//    }
-//    else if( findLine (line, 0, -spacing, 0, 0) && findLine (line, -spacing, 0, 0, 0) ){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.min(line.x1, line.x2), Math.min(line.y1, line.y2)]  
-//        console.log("slope -1 x-y- connected", triangle)
-//    }
-//  } 
-//  //line slope is 1, "backslash"
-//  if ( slope === 1 ){ 
-//    if(  findLine (line, 0, spacing, 0, 0) && findLine (line, -spacing, 0, 0, 0) ){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.min(line.x1, line.x2), Math.max(line.y1, line.y2)]  
-//        console.log("slope 1 x-y+ connected", triangle)
-//    }
-//    else if ( findLine (line, 0, -spacing, 0, 0) && findLine (line, spacing, 0, 0, 0)) {
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.min(line.y1, line.y2)]  
-//        console.log("slope 1 x-y- connected", triangle)
-//    }
-//  }
-//  //line slope is 0, horizontal line 
-//  if ( slope === -0 || slope === 0){ 
-//    if( findLine (line, 0, -spacing, 0, 0) && findLine (line, spacing, -spacing, 0, 0)){ 
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.min(line.y1, line.y2)-spacing]  
-//        console.log("slope 0 x-y+ connected", triangle)
-//    }
-//    else if( findLine (line, 0, spacing, 0, 0) && findLine (line, spacing, spacing, 0, 0)){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.max(line.y1, line.y2)+spacing]  
-//        console.log("slope 0 x-y- connected", triangle)
-//    }
-//  }
-//  if ( slope === Infinity || slope === -Infinity){}
-//    if( findLine (line, -spacing, 0, 0, 0) &&  findLine (line, -spacing, spacing, 0, 0)){ 
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.min(line.y1, line.y2)-spacing, Math.min(line.y1, line.y2)-spacing]  
-//        console.log("slope Inf x-y- connected", triangle)
-//    }
-//    else if( findLine (line, 0, spacing, 0, 0) && findLine (line, spacing, spacing, 0, 0)){ 
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.max(line.y1, line.y2)+spacing]  
-//        console.log("slope 0 x-y- connected", triangle)
-//    }
-//  return triangle
-//}
-//function findClosed (line) {
-//  slope = ( line.y2 - line.y1 ) / ( line.x2 - line.x1 )
-//  triangle = null
-//  //line slope is -1, "backslash"
-//  if ( slope === -1 ){ 
-//    if( ( findLine (line, 0, spacing, 0, 0) || findLine (line, 0, 0, 0, spacing))
-//      &&( findLine (line, spacing, 0, 0, 0) || findLine (line, 0, 0, spacing, 0))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.max(line.y1, line.y2)]  
-//        console.log("slope -1 x+y+ connected")
-//	    console.log(triangle)
-//    }
-//    else if(( findLine (line, 0, -spacing, 0, 0) || findLine (line, 0, 0, 0, -spacing))
-//      &&( findLine (line, -spacing, 0, 0, 0) || findLine (line, 0, 0, -spacing, 0))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.min(line.x1, line.x2), Math.min(line.y1, line.y2)]  
-//        console.log("slope -1 x-y- connected")
-//	    console.log(triangle)
-//    }
-//  } 
-//  //line slope is 1, "forwardslash"
-//  if ( slope === 1 ){ 
-//    if( ( findLine (line, 0, spacing, 0, 0) || findLine (line, 0, 0, 0, spacing))
-//      &&( findLine (line, -spacing, 0, 0, 0) || findLine (line, 0, 0, -spacing, 0))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.min(line.x1, line.x2), Math.max(line.y1, line.y2)]  
-//        console.log("slope 1 x-y+ connected")
-//	    console.log(triangle)
-//    }
-//    else if(( findLine (line, 0, -spacing, 0, 0) || findLine (line, 0, 0, 0, -spacing))
-//      &&( findLine (line, spacing, 0, 0, 0) || findLine (line, 0, 0, spacing, 0))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.min(line.y1, line.y2)]  
-//        console.log("slope 1 x-y- connected")
-//	    console.log(triangle)
-//    }
-//  }
-//  //line slope is 0, horizontal line 
-//  if ( slope === -0 || slope === 0){ 
-//    if( ( findLine (line, 0, -spacing, 0, 0) || findLine (line, 0, 0, 0, -spacing))
-//      &&( findLine (line, spacing, -spacing, 0, 0) || findLine (line, 0, 0, spacing, -spacing))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.min(line.y1, line.y2)-spacing]  
-//        console.log("slope 0 x-y+ connected")
-//	    console.log(triangle)
-//    }
-//    else if(( findLine (line, 0, spacing, 0, 0) || findLine (line, 0, 0, 0, spacing))
-//      &&( findLine (line, spacing, spacing, 0, 0) || findLine (line, 0, 0, spacing, spacing))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.max(line.y1, line.y2)+spacing]  
-//        console.log("slope 0 x-y- connected")
-//	    console.log(triangle)
-//    }
-//  }
-//  if ( slope === Infinity || slope === -Infinity){}
-//    if( ( findLine (line, -spacing, 0, 0, 0) || findLine (line, 0, 0, -spacing, 0))
-//      &&( findLine (line, -spacing, spacing, 0, 0) || findLine (line, 0, 0, -spacing, spacing))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.min(line.y1, line.y2)-spacing, Math.min(line.y1, line.y2)-spacing]  
-//        console.log("slope Inf x-y- connected")
-//	    console.log(triangle)
-//    }
-//    else if(( findLine (line, 0, spacing, 0, 0) || findLine (line, 0, 0, 0, spacing))
-//      &&( findLine (line, spacing, spacing, 0, 0) || findLine (line, 0, 0, spacing, spacing))){
-//        triangle = [line.x1, line.y1, line.x2, line.y2, Math.max(line.x1, line.x2), Math.max(line.y1, line.y2)+spacing]  
-//        console.log("slope 0 x-y- connected")
-//	    console.log(triangle)
-//    }
-//  return triangle
-//}
+
 function findLine ( line, x1a, y1a, x2a, y2a ){
   return lines_aa[[[(line.x1 + x1a), (line.y1 + y1a)], [(line.x2 + x2a), (line.y2 + y2a)]].sort().join(',')]  
 }
+
 function mutateLine (line, xSpacing, ySpacing) {
   return { yLine1 : [line.x1, (line.y1 + ySpacing), line.x2, line.y2].join(',') , 
 	   yLine2 : [line.x1, line.y1, line.x2, (line.y2 + ySpacing)].join(',') , 
@@ -352,11 +252,13 @@ function mutateLine (line, xSpacing, ySpacing) {
 	   xLine2 : [line.x1, line.y1, (line.x2 + xSpacing), line.y2].join(',')
   }
 }
+
 function validate (line) {
   return validateNode(line.x1, line.y1) &&
 		validateNode(line.x2, line.y2) &&
 		validateLine(line.x1, line.y1, line.x2, line.y2)
 }
+
 function validateNode (x, y) {
   return (x >= spacing &&
 		x <= gridWidth - spacing &&
